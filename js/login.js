224f859cb714b650e79d34bb21147e8b760baeca
end360 <end360@users.noreply.github.com>
2020-04-22 18:57:17 -0400

    Update login.js
    Moved inline script to script file.

--- js/login.js
+++ js/login.js
@@ -1,13 +1,26 @@
-const form = document.getElementById("form");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-
-// Get Field Name
-function getFieldName(input) {
-  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+function handleLogin() {
+	var resp = JSON.parse(this.response);
+	var div = document.forms[0].children.message;
+	if(resp.message) {
+		div.innerText = resp.message;
+	}
+	if(resp.success) {
+		div.className = "message success";
+		setTimeout(function(){ window.location.href = "index.php"; }, 1500);
+	} else {
+		div.className = "message error";
+	}
 }
-
+function doLogin(form) {
+	form.children.message.innerText = "";
+	var xhr = new XMLHttpRequest();
+	xhr.addEventListener("load", handleLogin);
+	xhr.open("POST", "api/login.php", true);
+	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	xhr.send(encodeURI("email=" + form.elements.login_email.value + "&password=" + form.elements.login_password.value));
+}
 // Submit Event Listener
-form.addEventListener("submit", function (e) {
+document.forms[0].addEventListener("submit", function (e) {
+	doLogin(this);
   e.preventDefault();
 });
